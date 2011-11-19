6c9c924703ac21f991bd242bcec4a2b0267a1a04
Miltiadis Allamanis <mallamanis@gmail.com>
2011-11-19 17:36:55 +0000

    MemoryUnsafePasswordStore initial commit

---
+++ passgoriLibrary/src/MemoryUnsafePasswordStore.java
@@ -0,0 +1,71 @@
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * 
+ */
+
+/**
+ * A memory unsafe password store.
+ * 
+ * @author Miltiadis Allamanis
+ * 
+ */
+public class MemoryUnsafePasswordStore implements IPasswordStore {
+
+	/**
+	 * A hash map storing the passwords.
+	 */
+	private final HashMap<String, Password> mPasswordStore;
+
+	public MemoryUnsafePasswordStore() {
+		mPasswordStore = new HashMap<String, Password>();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see IPasswordStore#getAllStoredPasswordIds()
+	 */
+	@Override
+	public AbstractList<String> getAllStoredPasswordIds()
+			throws PasswordStoreException {
+		final Set<String> idSet = mPasswordStore.keySet();
+		return new ArrayList<String>(idSet);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see IPasswordStore#removePassword(java.lang.String)
+	 */
+	@Override
+	public boolean removePassword(String aId) throws PasswordStoreException {
+		return mPasswordStore.remove(aId) != null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see IPasswordStore#retrivePassword(java.lang.String)
+	 */
+	@Override
+	public Password retrivePassword(String aId) throws PasswordStoreException {
+		return mPasswordStore.get(aId);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see IPasswordStore#storePassword(Password)
+	 */
+	@Override
+	public boolean storePassword(Password aPassword)
+			throws PasswordStoreException {
+
+		return mPasswordStore.put(aPassword.getId(), aPassword) != null;
+	}
+
+}
