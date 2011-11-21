b52d26f10d66ddd919c2ad6c607277d821d4343a
Miltiadis Allamanis <mallamanis@gmail.com>
2011-11-21 17:42:30 +0000

    added loading from password store

--- PassgoriApp/src/ac/uk/cam/cl/passgroiApp/PassgoriPresentPasswordsActivity.java
+++ PassgoriApp/src/ac/uk/cam/cl/passgroiApp/PassgoriPresentPasswordsActivity.java
@@ -3,8 +3,19 @@
  */
 package ac.uk.cam.cl.passgroiApp;
 
+import uk.ac.cam.cl.passgori.IPasswordStore;
+import uk.ac.cam.cl.passgori.Password;
+import uk.ac.cam.cl.passgori.PasswordStoreException;
+import ac.uk.cam.cl.passgroiApp.PasswordStoreService.PasswordStorageBinder;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -15,10 +26,49 @@ import android.widget.TextView;
  */
 public class PassgoriPresentPasswordsActivity extends Activity {
 
-	/**
-	 * TODO: Remove
-	 */
-	private String currentPasswordTitle;
+	private class GetPassword extends Thread {
+		@Override
+		public void run() {
+			try {
+				Password password = mPasswordStore.retrivePassword(getIntent()
+						.getExtras().getString("passwordId"));
+
+				if (password != null) {
+					UpdatePasswordDetails updater = new UpdatePasswordDetails(
+							password);
+					runOnUiThread(updater);
+				} else {
+					// Update GUI about failure!!
+				}
+			} catch (PasswordStoreException e) {
+				// TODO Inform GUI
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private class UpdatePasswordDetails implements Runnable {
+
+		private final Password mPassword;
+
+		public UpdatePasswordDetails(Password password) {
+			mPassword = password;
+		}
+
+		@Override
+		public void run() {
+			mUsernameField.setText(mPassword.getUsername());
+			mPasswordField.setText(mPassword.getPassword());
+			mNotesField.setText(mPassword.getNotes());
+
+			mUsernameField.setVisibility(View.VISIBLE);
+			mPasswordField.setVisibility(View.VISIBLE);
+			mNotesField.setVisibility(View.VISIBLE);
+
+			mLoadingDialog.dismiss();
+		}
+
+	}
 
 	/**
 	 * The TextView corresponding to the password's title.
@@ -40,6 +90,38 @@ public class PassgoriPresentPasswordsActivity extends Activity {
 	 */
 	private TextView mNotesField;
 
+	/**
+	 * Progress Dialog to indicate progress.
+	 */
+	private ProgressDialog mLoadingDialog;
+
+	/**
+	 * The password store.
+	 */
+	private IPasswordStore mPasswordStore;
+
+	/** Defines callbacks for service binding, passed to bindService() */
+	private final ServiceConnection mConnection = new ServiceConnection() {
+
+		private boolean mBound;
+
+		@Override
+		public void onServiceConnected(ComponentName className, IBinder service) {
+			PasswordStorageBinder binder = (PasswordStorageBinder) service;
+			mPasswordStore = binder.getStore();
+			mBound = true;
+
+			// Spawn thread to get password
+			new GetPassword().start();
+
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName arg0) {
+			mBound = false;
+		}
+	};
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -51,7 +133,13 @@ public class PassgoriPresentPasswordsActivity extends Activity {
 		mPasswordField = (TextView) findViewById(R.id.passwordField);
 		mNotesField = (TextView) findViewById(R.id.notesField);
 
-		currentPasswordTitle = getIntent().getExtras().getString("passwordId");
+		mLoadingDialog = ProgressDialog.show(this, "",
+				"Loading. Please wait...", true);
+
+		mPasswordTitle.setText(getIntent().getExtras().getString("passwordId"));
+		mUsernameField.setVisibility(View.INVISIBLE);
+		mPasswordField.setVisibility(View.INVISIBLE);
+		mNotesField.setVisibility(View.INVISIBLE);
 	}
 
 	@Override
@@ -60,6 +148,21 @@ public class PassgoriPresentPasswordsActivity extends Activity {
 		// Close activity to avoid storing the password if someone exits us from
 		// the home button
 		finish();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		// Bind to PasswordStoreService
+		Intent intent = new Intent(this, PasswordStoreService.class);
+
+		if (!getApplicationContext().bindService(intent, mConnection,
+				Context.BIND_AUTO_CREATE)) {
+			// Inform GUI for failure!!
+		}
+
+		// Once the service is binded, we will load the list
 	}
 
 }
