945c477ec48700f70ac33b2f6507197693ef86e9
Daniel Thomas <drt24@cam.ac.uk>
2012-03-19 11:21:52 +0000

    Use deleteDir from Util to do recursive deletion
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- library/src/test/java/uk/ac/cam/cl/passgori/NigoriPasswordStoreTest.java
+++ library/src/test/java/uk/ac/cam/cl/passgori/NigoriPasswordStoreTest.java
@@ -47,6 +47,7 @@ import com.google.nigori.common.Index;
 import com.google.nigori.common.NigoriCryptographyException;
 import com.google.nigori.common.RevValue;
 import com.google.nigori.common.UnauthorisedException;
+import com.google.nigori.common.Util;
 
 /**
  * 
@@ -96,34 +97,11 @@ public class NigoriPasswordStoreTest {
   @After
   public void deleteDatabase() {
     if (je.exists()){
-      deleteDir(je);
+      Util.deleteDir(je);
       je.delete();
     }
   }
 
-  /**
-   * Deletes all files and subdirectories under dir. Returns true if all deletions were successful.
-   * If a deletion fails, the method stops attempting to delete and returns false.
-   * 
-   * From http://www.exampledepot.com/egs/java.io/DeleteDir.html
-   * 
-   * @param dir
-   * @return
-   */
-  public static boolean deleteDir(File dir) {
-    if (dir.isDirectory()) {
-      String[] children = dir.list();
-      for (int i = 0; i < children.length; i++) {
-        boolean success = deleteDir(new File(dir, children[i]));
-        if (!success) {
-          return false;
-        }
-      }
-    }
-
-    // The directory is now empty so delete it
-    return dir.delete();
-  }
 
   @Test
   public void testNigoriConnection() throws IOException, NigoriCryptographyException,
